0d4abb6c5bf5b2bece0b6386ab74233e5fcbfee0
viren <98017229+viren16@users.noreply.github.com>
2023-12-06 01:34:10 +0530

    updated the unwanted file

--- app/safe/validationSchema.js
+++ app/safe/validationSchema.js
@@ -39,27 +39,4 @@ export default [
     addressoptional: Yup.string(),
     bylines: Yup.string(),
   }),
-  // Yup.object().shape({
-  //   [nameOnCard.name]: Yup.string().required(`${nameOnCard.requiredErrorMsg}`),
-  //   [cardNumber.name]: Yup.string()
-  //     .required(`${cardNumber.requiredErrorMsg}`)
-  //     .matches(visaRegEx, cardNumber.invalidErrorMsg),
-  //   [expiryDate.name]: Yup.string()
-  //     .nullable()
-  //     .required(`${expiryDate.requiredErrorMsg}`)
-  //     .test('expDate', expiryDate.invalidErrorMsg, val => {
-  //       if (val) {
-  //         const startDate = new Date();
-  //         const endDate = new Date(2050, 12, 31);
-  //         if (moment(val, moment.ISO_8601).isValid()) {
-  //           return moment(val).isBetween(startDate, endDate);
-  //         }
-  //         return false;
-  //       }
-  //       return false;
-  //     }),
-  //   [cvv.name]: Yup.string()
-  //     .required(`${cvv.requiredErrorMsg}`)
-  //     .test('len', `${cvv.invalidErrorMsg}`, val => val && val.length === 3)
-  // })
 ];
